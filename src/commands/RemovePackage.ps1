2668e14a119764fe82b64a84c6561aafeb18e9fe
per_terra <53376589+Per-Terra@users.noreply.github.com>
2024-01-05 17:40:46 +0900

    空のフォルダを削除する処理を追加

--- src/commands/RemovePackage.ps1
+++ src/commands/RemovePackage.ps1
@@ -77,6 +77,18 @@ if (Test-Path -LiteralPath $PackageDirectory -PathType Container) {
   }
 }
 
+Write-Debug -Message "空のディレクトリを削除しています: $RootDirectory"
+Get-ChildItem -LiteralPath $RootDirectory -Directory -Recurse -Force | Sort-Object -Descending | ForEach-Object {
+  if ((Get-ChildItem -LiteralPath $_.FullName -Force).Count -eq 0) {
+    try {
+      Remove-Item -LiteralPath $_.FullName -Force
+    }
+    catch {
+      Write-Error -Message "ディレクトリの削除に失敗しました: $($_.FullName)"
+    }
+  }
+}
+
 try {
   (($managedFiles | ConvertTo-Csv -NoTypeInformation) -join "`n") + "`n" | Set-Content -LiteralPath $ManagedFilesPath -Force -NoNewline
 }
